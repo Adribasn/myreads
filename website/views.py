732f4c153703706541fa5c7dcb59c488a4688891
Adrien Basyn <adrien.basyn@outlook.com>
2023-03-20 17:30:53 +0100

    basic search bar with google books api

--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_login import login_required, current_user
+import requests
 
 views = Blueprint('views', __name__)
 
@@ -7,3 +8,20 @@ views = Blueprint('views', __name__)
 @login_required
 def home():
     return render_template('home.html', user=current_user)
+
+api_key = 'AIzaSyBsxxDP5ZK-lDQ-6N4VtEB96eMtefiS5WE'
+
+@views.route('/search', methods=['GET', 'POST'])
+@login_required
+def search():
+    if request.method == 'POST':
+        book = request.form.get('book')
+
+        if book:
+            url = f'https://www.googleapis.com/books/v1/volumes?q={book}:keyes&key={api_key}'
+            r = requests.get(url)
+            data = r.json()
+            for element in data['items']:
+                print(element['volumeInfo']['title'])
+        
+    return render_template('search.html', user=current_user)
