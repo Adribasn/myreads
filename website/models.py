adf9ef44f02798b9898b37e7da9ac9e0b43d4c43
Adrien Basyn <adrien.basyn@outlook.com>
2023-03-22 15:53:40 +0100

    Now able to pass book element data from html to python

--- website/models.py
+++ website/models.py
@@ -6,3 +6,14 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String, unique=True)
     firstName = db.Column(db.String)
     password = db.Column(db.String)
+    books = db.relationship('Book')
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    authors = db.Column(db.String)
+    publisher = db.Column(db.String)
+    date = db.Column(db.String)
+    description = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
--- website/views.py
+++ website/views.py
@@ -9,21 +9,33 @@ views = Blueprint('views', __name__)
 def home():
     return render_template('home.html', user=current_user)
 
-api_key = 'AIzaSyBsxxDP5ZK-lDQ-6N4VtEB96eMtefiS5WE'
-
 @views.route('/search', methods=['GET', 'POST'])
 @login_required
 def search():
     if request.method == 'POST':
-        book = request.form.get('book')
+        formType = request.form.get('formType')
 
-        if book:
-            url = f'https://www.googleapis.com/books/v1/volumes?q={book}&langRestrict=en'
-            r = requests.get(url)
-            data = r.json()
-            for element in data['items']:
-                print(element['volumeInfo'])
-            
-            return render_template('search.html', user=current_user, data=data)
+        if formType == 'searchForm':
+            book = request.form.get('book')
+
+            if book:
+                url = f'https://www.googleapis.com/books/v1/volumes?q={book}&langRestrict=en'
+                r = requests.get(url)
+                data = r.json()
+                for element in data['items']:
+                    print(element['volumeInfo'])
+                
+                return render_template('search.html', user=current_user, data=data)
+        else:
+            title = request.form.get('title')
+            authors = request.form.get('authors')
+            publisher = request.form.get('publisher')
+            date = request.form.get('date')
+            description = request.form.get('description')
+            print(title)
+            print(authors)
+            print(publisher)
+            print(date)
+            print(description)
         
     return render_template('search.html', user=current_user)
